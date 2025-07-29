95baaedf5ef2dd47cfd609506c161292f9ef0ed6
AleenaFernz <aleena.ferz@gmail.com>
2025-07-29 22:46:47 +0530

    Setup complete: Integrated lume-nlp and FastAPI mood detection endpoint

--- lume-nlp/setup.py
+++ lume-nlp/setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lume_nlp" ,
-    version="0.1" ,
+    name="lume_nlp",
+    version="0.1",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "transformers",
+        "torch",
+        "numpy",
+        "pandas",
+        "scikit-learn"
+    ],
 )
